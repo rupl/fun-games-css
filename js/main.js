14ee1f7eb206a27ab8b821e73e94a090a1b661bf
Chris Ruppel <chris.ruppel@gmail.com>
2013-03-05 23:47:28 -0600

    adding slide about CSS modules. Adjusted 'history' slide a little.

--- js/main.js
+++ js/main.js
@@ -42,7 +42,26 @@
       "$('#s-intro .word').toggleClass('gradient transition'); ", // sneak transition in there too
       "$('#s-intro .word').toggleClass('webfont'); ",
       "$('#s-intro .word').toggleClass('rotate'); ",
+      "$('#s-intro .word').toggleClass('invert'); ",
       "$('#s-intro .word').toggleClass('blur'); ",
+
+      // What is CSS?
+      "scrollTo('#s-css'); ",
+      "$('#s-css .list').toggleClass('show'); ",
+      "$('#s-css .list').toggleClass('show'); "+
+      "$('#s-css .specs').toggleClass('show'); "+
+      "$('#s-css .finished').toggleClass('show'); ",
+      "$('#s-css .everything').toggleClass('show'); ",
+      "$('#s-css .slash').toggleClass('drop'); ",
+      "$('#s-css .finished').toggleClass('show'); "+
+      "$('#s-css .everything').toggleClass('show'); "+
+      "$('#s-css .unfinished').toggleClass('show'); ",
+      "$('#s-css .drafts').toggleClass('smaller'); ",
+      "$('#s-css .drafts').toggleClass('even-smaller'); ",
+      "$('#s-css .drafts').toggleClass('columns'); ",
+      "$('#s-css .drafts').toggleClass('highlight'); "+
+      "$('#s-css .fun').toggleClass('show'); "+
+      "$('#s-css .covering').toggleClass('show'); ",
 
       // Slinky
       "scrollTo('#s-slinky'); ",
@@ -69,7 +88,7 @@
       "$('#s-intro .word').toggleClass('bold'); ",
       "$('#s-intro .word').toggleClass('italic'); ",
       "$('#s-intro .word').toggleClass('font'); ",
-      "$('#s-intro p.intro').text('Web design had pretty humble beginnings.'); ",
+      "$('#s-intro p.intro').text('CSS used to be defined in huge chunks (CSS 1, CSS 2, CSS 2.1)'); ",
       "$('#s-intro .word').toggleClass('border'); ",
       "$('#s-intro .word').toggleClass('padding'); ",
       "$('#s-intro .word').toggleClass('margin'); ",
@@ -81,19 +100,38 @@
       "$('#s-intro .word').toggleClass('pos-y'); ",
       "$('#s-intro .word').toggleClass('pos-x'); ",
       "$('#s-intro .word').toggleClass('pos-x'); ",
-      "$('#s-intro p.intro').text('CSS used to be defined in huge chunks (CSS 1, CSS 2, CSS 2.1)'); ",
-      "$('#s-intro p.intro').text('This is about where IE6 gave up.'); "+
-      "$('#s-intro .word').text('CSS'); ",
+      "$('#s-intro p.intro').text('This is about where IE6 gave up.'); ",
+      "$('#s-intro p.intro').text('After CSS 2.1, CSS was broken up into modules, known collectively as...'); "+
+      "$('#s-intro .word').text('CSS3'); ",
       "$('#s-intro .word').toggleClass('borderradius'); ",
       "$('#s-intro .word').toggleClass('boxshadow'); ",
       "$('#s-intro .word').toggleClass('textshadow'); ",
       "$('#s-intro .word').toggleClass('gradient transition'); ", // sneak transition in there too
       "$('#s-intro .word').toggleClass('webfont'); ",
       "$('#s-intro .word').toggleClass('rotate'); ",
+      "$('#s-intro .word').toggleClass('invert'); ",
       "$('#s-intro .word').toggleClass('blur'); ",
 
-      // Slinky
+      // What is CSS?
       "scrollTo('#s-intro'); ",
+      "$('#s-css .list').toggleClass('show'); ",
+      "$('#s-css .list').toggleClass('show'); "+
+      "$('#s-css .specs').toggleClass('show'); "+
+      "$('#s-css .finished').toggleClass('show'); ",
+      "$('#s-css .everything').toggleClass('show'); ",
+      "$('#s-css .slash').toggleClass('drop'); ",
+      "$('#s-css .finished').toggleClass('show'); "+
+      "$('#s-css .everything').toggleClass('show'); "+
+      "$('#s-css .unfinished').toggleClass('show'); ",
+      "$('#s-css .drafts').toggleClass('smaller'); ",
+      "$('#s-css .drafts').toggleClass('even-smaller'); ",
+      "$('#s-css .drafts').toggleClass('columns'); ",
+      "$('#s-css .drafts').toggleClass('highlight'); "+
+      "$('#s-css .fun').toggleClass('show'); "+
+      "$('#s-css .covering').toggleClass('show'); ",
+
+      // Slinky
+      "scrollTo('#s-css'); ",
       "$('#s-slinky .slinky').toggleClass('hide'); ",
       "$('#s-slinky .slinky').toggleClass('half'); ",
       "$('#s-slinky p.slinky-desc').toggleClass('hide'); "+
